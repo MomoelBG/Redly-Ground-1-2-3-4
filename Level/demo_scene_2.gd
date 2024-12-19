c9c814568ccb13491f7cb430f6fe7ffcb07051fc
MomoelBG <mohamed.sadek1605@gmail.com>
2024-12-19 22:12:56 +0100

    f

--- Level/demo_scene_2.gd
+++ Level/demo_scene_2.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @onready var gate1 = $Puzzles/Gate25
 @onready var gate2
-@onready var ppc4_1 = $PPC4scanner
+@onready var ppc4_1
 
 
 
@@ -10,4 +10,4 @@ func _on_button_5_pressed():
 	gate1._open()
 
 func _on_power_reciever_power_recieved():
-	ppc4_1.powered = true
+	pass
